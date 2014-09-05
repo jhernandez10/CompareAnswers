4623b86b0f7f961551094b9ceb6766e18a9493e4
jhernandez10 <j_hernandez10@utexas.edu>
2014-09-05 12:30:07 -0500

    Added Testing files
    Reorganized code and added testing files

--- compareAnswers.py
+++ compareAnswers.py
@@ -1,5 +1,39 @@
 import sys
 
+def compare(answers,output, report):
+	line_counter = 0
+	errors = 0
+
+	line1 = file1.readline()
+	line2 = file2.readline()
+
+	#read and compare lines until one or both of the files finish
+	while(line1 != "" and line2 != ""):
+		#if the are not equal write them to a file
+		if(line1 != line2):
+			reason = "Line" + str(line_counter) + " does not match:\t" + line1 + " | \t" + line2 
+			report.write(reason)
+			errors += 1
+		line_counter += 1
+		line1 = file1.readline()
+		line2 = file2.readline()
+
+	#make sure one of the files doesn't have more lines
+	if(line1 != ""):
+		while(line1 != ""):
+			reason = "Line" + str(line_counter) + " does not match:\t" + line1 + " | \t" + line2 
+			report.write()
+			errors += 1
+			line_counter += 1
+			line1 = line1.readline()
+	elif(line2 != ""):
+		while(line1 != ""):
+			reason = "Line" + str(line_counter) + " does not match:\t" + line1 + " | \t" + line2 
+			report.write()
+			errors += 1
+			line_counter += 1
+			line2 = line2.readline()
+
 #takes in two files one of known answers and other of produced output
 #answers and output are both one per line
 def main():
@@ -14,23 +48,5 @@ def main():
 	#open a new file to write the differences to
 	report = open("report.txt", 'w')
 
-	line_counter = 0
-	errors = 0
-
-	line1 = file1.readline()
-	line2 = file2.readline()
-
-	#read and compare lines until one or both of the files finish
-	while(line1 != "" and line2 != ""):
-		#if the are not equal write them to a file
-		if(line1 != line2):
-			reason = line1 + " " + line2 + " on line" + str(line_counter) + " do not match"
-			report.write(reason)
-			errors += 1
-		line_counter += 1
-		line1 = file1.readline()
-		line2 = file2.readline()
-
-	#make sure one of the files doesn't have more lines
-	#if(line1 != ""):
+	compare(file1,file2,report)
 
